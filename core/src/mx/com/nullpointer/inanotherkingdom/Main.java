f4b0425a65a1ba4952437f2d7637dffbc8e715f2
carlos-cecnn12 <a01373264@itesm.mx>
2018-02-06 13:53:34 -0600

    Corrección del comentario :)

--- core/src/mx/com/nullpointer/inanotherkingdom/Main.java
+++ core/src/mx/com/nullpointer/inanotherkingdom/Main.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 /*Hola Charly*/
-
+*
 public class Main extends Game {
     SpriteBatch batch;
     Texture img;
