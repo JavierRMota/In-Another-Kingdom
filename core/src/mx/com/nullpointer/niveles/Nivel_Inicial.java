328b101b6a676dfc3e7136e27efe19c26a4e1d64
JavierRMota <A01372812@itesm.mx>
2018-02-13 13:25:32 -0600

    Added first level screen

--- core/src/mx/com/nullpointer/niveles/Nivel_Inicial.java
+++ core/src/mx/com/nullpointer/niveles/Nivel_Inicial.java
@@ -1,9 +1,44 @@
 package mx.com.nullpointer.niveles;
 
+import com.badlogic.gdx.Screen;
+
 /**
  * Created by mota on 2/12/18.
  */
 
-public class Nivel_Inicial {
-    //Blabla
+public class Nivel_Inicial implements Screen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
