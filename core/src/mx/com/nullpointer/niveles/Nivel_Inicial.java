9ceac35dc5c67ba7a28c08062c6e42c421622c29
JavierRMota <A01372812@itesm.mx>
2018-02-14 10:17:46 -0600

    Added First level functionality and main character animation

--- core/src/mx/com/nullpointer/niveles/Nivel_Inicial.java
+++ core/src/mx/com/nullpointer/niveles/Nivel_Inicial.java
@@ -1,12 +1,34 @@
 package mx.com.nullpointer.niveles;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import mx.com.nullpointer.inanotherkingdom.Main;
+import mx.com.nullpointer.utils.MainCharacter;
 
 /**
  * Created by mota on 2/12/18.
  */
 
 public class Nivel_Inicial implements Screen {
+    private final Main game;
+
+    //Mapas
+    private TiledMap tiledMap;
+    private OrthogonalTiledMapRenderer render;
+    private MainCharacter laurence;
+
+    //Cámaras
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private Stage buttons;
+    private Stage pause;
+
+    public Nivel_Inicial(Main game){this.game =game;}
     @Override
     public void show() {
 
--- core/src/mx/com/nullpointer/utils/MainCharacter.java
+++ core/src/mx/com/nullpointer/utils/MainCharacter.java
@@ -1,6 +1,11 @@
 package mx.com.nullpointer.utils;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 /**
  * Created by mota on 2/13/18.
@@ -12,6 +17,67 @@ public class MainCharacter extends Objeto
     private float timerAnimation;
     private float x,y; //Coordenadas de dónde se moverá
     //Tamaño frames: 13.97cm * 8.29cm
+
+    //Estados de movimiento
+    MovementState movementState = MovementState.RUNNING;
+    private static final float VX = 240;
+    public MainCharacter(Texture texture)
+    {
+        TextureRegion region = new TextureRegion(texture);
+        TextureRegion[][] characterTexture = region.split(396,235);
+        animation = new Animation(0.15f,characterTexture[0][0],characterTexture[0][1],characterTexture[0][2]);
+        animation.setPlayMode(Animation.PlayMode.LOOP);
+        timerAnimation =0;
+        // Quieto
+        sprite = new Sprite(characterTexture[0][0]);
+        sprite.setPosition(0,64);
+        x = 0;
+        y = 64;
+    }
+    public void render(SpriteBatch batch) {
+        if (movementState==MovementState.STANDING) {
+            //batch.draw(marioQuieto.getTexture(),x,y);
+            sprite.draw(batch);
+        } else {
+            timerAnimation += Gdx.graphics.getDeltaTime();
+            TextureRegion region = (TextureRegion) animation.getKeyFrame(timerAnimation);
+            batch.draw(region, x, y);
+        }
+    }
+    public float getX()
+    {
+        return x;
+    }
+    public float getY()
+    {
+        return y;
+    }
+    public void setMovementState(MovementState movment)
+    {
+        this.movementState = movment;
+    }
+    public void setX(float x)
+    {
+        this.x=x;
+    }
+    public void setY(float y)
+    {
+        this.y =y;
+    }
+    public void move(float dx) {
+        this.x += dx;
+        sprite.setPosition(x, y);
+    }
+
+    public enum MovementState
+    {
+        RUNNING,
+        JUMPING,
+        DODGING,
+        STANDING
+    }
+
+
 
 
 
