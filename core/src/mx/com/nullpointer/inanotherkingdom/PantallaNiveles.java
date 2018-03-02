b898e7e5077b562cdabec09bd771ffbc84410055
MarinaHaro <A01373471@itesm.mx>
2018-03-01 22:35:56 -0600

    Pantala niveles

--- core/src/mx/com/nullpointer/inanotherkingdom/PantallaNiveles.java
+++ core/src/mx/com/nullpointer/inanotherkingdom/PantallaNiveles.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
+import mx.com.nullpointer.niveles.Nivel_Inicial;
 import mx.com.nullpointer.utils.GenericScreen;
 import mx.com.nullpointer.utils.Text;
 
@@ -28,6 +29,7 @@ class PantallaNiveles extends GenericScreen{
     //Texturas
     private Texture texturaFondo;
     private Text msg;
+    private Texture texturaTitulo;
 
 
 
@@ -45,6 +47,7 @@ class PantallaNiveles extends GenericScreen{
     private void cargarTexturas() {
         texturaFondo = new Texture("background/menubg.png");
         msg = new Text();
+        texturaTitulo = new Texture("logo_alargado.png");
 
     }
 
@@ -68,6 +71,59 @@ class PantallaNiveles extends GenericScreen{
         escenaNiveles.addActor(btnBack);
         Gdx.input.setInputProcessor(escenaNiveles);
 
+        //Botón Primer Nivel
+        TextureRegionDrawable trdFirst = new TextureRegionDrawable(new TextureRegion(new Texture("niveles/uno/levelOneBook.png")));
+        TextureRegionDrawable trdFirstPress = new TextureRegionDrawable(new TextureRegion(new Texture("niveles/uno/levelOneBook.png")));
+        ImageButton btnFirst = new ImageButton(trdFirst,trdFirstPress);
+        btnFirst.setPosition(btnFirst.getWidth(),PantallaMenu.ALTO/2 - btnFirst.getHeight()/2);
+
+        btnFirst.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                super.clicked(event, x, y);
+                game.setScreen(new Nivel_Inicial(game));
+
+            }
+        });
+        escenaNiveles.addActor(btnFirst);
+        Gdx.input.setInputProcessor(escenaNiveles);
+
+        //Botón Segundo Nivel
+        TextureRegionDrawable trdSec = new TextureRegionDrawable(new TextureRegion(new Texture("niveles/lockedBook.png")));
+        TextureRegionDrawable trdSecPress = new TextureRegionDrawable(new TextureRegion(new Texture("niveles/lock.png")));
+        ImageButton btnSec = new ImageButton(trdSec,trdSecPress);
+        btnSec.setPosition(3*btnSec.getWidth(),PantallaMenu.ALTO/2 - btnSec.getHeight()/2);
+
+        btnSec.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                super.clicked(event, x, y);
+                game.setScreen(new PantallaNiveles(game));
+
+            }
+        });
+        escenaNiveles.addActor(btnSec);
+        Gdx.input.setInputProcessor(escenaNiveles);
+
+        //Botón Tercer Nivel
+        TextureRegionDrawable trdThird = new TextureRegionDrawable(new TextureRegion(new Texture("niveles/lockedBook.png")));
+        TextureRegionDrawable trdThirdPress = new TextureRegionDrawable(new TextureRegion(new Texture("niveles/lock.png")));
+        ImageButton btnThird = new ImageButton(trdThird,trdThirdPress);
+        btnThird.setPosition(5*btnThird.getWidth(),PantallaMenu.ALTO/2 - btnThird.getHeight()/2);
+
+        btnThird.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                super.clicked(event, x, y);
+                game.setScreen(new PantallaNiveles(game));
+
+            }
+        });
+        escenaNiveles.addActor(btnThird);
+        Gdx.input.setInputProcessor(escenaNiveles);
+
+
+
     }
 
 
@@ -80,8 +136,9 @@ class PantallaNiveles extends GenericScreen{
 
         batch.begin();
         batch.draw(texturaFondo,0 ,0);
-        msg.showMsg(batch,"Niveles",ANCHO/2,3*ALTO/4,2);
-        msg.showMsg(batch,"Proximamente",ANCHO/2,ALTO/2,1);
+        //msg.showMsg(batch,"Niveles",ANCHO/2,3*ALTO/4,2);
+        //msg.showMsg(batch,"Proximamente",ANCHO/2,ALTO/2,1);
+        batch.draw(texturaTitulo, ANCHO/2 - texturaTitulo.getWidth()/2, 6*ALTO/8 );
         batch.end();
         escenaNiveles.draw();
 
