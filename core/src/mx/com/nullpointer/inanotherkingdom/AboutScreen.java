6ebeb370e986549606fcc21d883ffecceb1b4753
Berenice <berealamo@hotmail.com>
2018-03-30 13:29:46 -0600

    Changes in AboutScreen

--- core/src/mx/com/nullpointer/inanotherkingdom/AboutScreen.java
+++ core/src/mx/com/nullpointer/inanotherkingdom/AboutScreen.java
@@ -24,66 +24,31 @@ class AboutScreen extends GenericScreen {
 
     //Texturas
     private Texture backgroundTexture;
+    private Texture bannerTexture;
+    private Texture bere, charly, eli, javier, marina;
     private Text msg;
 
 
-    public AboutScreen(Main game)
-    {
+    public AboutScreen(Main game) {
         super(game);
     }
 
     @Override
     public void show() {
-        cargarTexturas();
-        crearObjetos();
+        loadTextures();
+        createObjects();
 
     }
 
-    private void cargarTexturas() {
-        backgroundTexture = new Texture("background/menubg.png");
+    private void loadTextures() {
+        backgroundTexture = assetManager.get("background/menubg.png");
+        bannerTexture = assetManager.get("screens/avatarBanner.png");
+        bere = assetManager.get("screens/avatarBere.png");
+        charly = assetManager.get("screens/avatarCharly.png");
+        eli = assetManager.get("screens/avatarEli.png");
+        javier = assetManager.get("screens/avatarJavier.png");
+        marina = assetManager.get("screens/avatarMarina.png");
         msg = new Text();
 
     }
-
-    private void crearObjetos() {
-        aboutStage = new Stage(view);
-
-        //Botón Back
-        TextureRegionDrawable trdBack = new TextureRegionDrawable(new TextureRegion(new Texture("btn/backbtn.png")));
-        TextureRegionDrawable trdBackPress = new TextureRegionDrawable(new TextureRegion(new Texture("btn/backbtnpress.png")));
-        ImageButton btnBack = new ImageButton(trdBack,trdBackPress);
-        btnBack.setPosition(btnBack.getWidth()*2, MenuScreen.HEIGHT /2 - btnBack.getHeight()/2);
-
-        btnBack.addListener(new ClickListener(){
-            @Override
-            public void clicked(InputEvent event, float x, float y){
-                super.clicked(event, x, y);
-                game.setScreen(new LoadingScreen(game, MENU));
-
-            }
-        });
-        aboutStage.addActor(btnBack);
-        Gdx.input.setInputProcessor(aboutStage);
-
-    }
-
-
-    @Override
-    public void render(float delta) {
-        clearScreen();
-
-        batch.setProjectionMatrix(camera.combined);
-
-        batch.begin();
-        batch.draw(backgroundTexture,0 ,0);
-        msg.showMsg(batch,"Informacion", WIDTH /2,3* HEIGHT /4,2);
-        msg.showMsg(batch,"Proximamente", WIDTH /2, HEIGHT /2,1);
-        batch.end();
-        aboutStage.draw();
-
-
-    }
-
-
-
 }
